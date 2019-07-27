866a7ce1b21874124c0f040be3e3e278d11fd688
Chidi Orji <orjichidi95@gmail.com>
2019-07-27 23:50:00 +0100

    feat: implement mark accepted function

--- shop/rules.py
+++ shop/rules.py
@@ -8,6 +8,7 @@ rules.add_rule('edit_customer', predicates.edit_customer)
 # job
 rules.add_rule('create_job', predicates.create_job)
 rules.add_rule('edit_job', predicates.edit_job)
+rules.add_rule('mark_accepted', predicates.mark_accepted)
 
 # cashflow
 rules.add_rule('create_cashflow', predicates.create_cashflow)
